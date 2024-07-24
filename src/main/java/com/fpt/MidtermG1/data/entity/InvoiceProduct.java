23b07ebe81e46420a66870e1af0f8a653dae4787
manuellaiv <13521051@std.stei.itb.ac.id>
2024-07-24 07:44:03 +0700

    feat: invoiceproductid entity

--- src/main/java/com/fpt/MidtermG1/data/entity/InvoiceProduct.java
+++ src/main/java/com/fpt/MidtermG1/data/entity/InvoiceProduct.java
@@ -4,15 +4,7 @@ import java.math.BigDecimal;
 
 import com.fpt.MidtermG1.dto.InvoiceProductDTO;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,15 +16,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@IdClass(InvoiceProductId.class)
 public class InvoiceProduct {
     @Id
     @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private int id;
 
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "invoice_id", nullable = false)
     private Invoice invoice;
 
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
