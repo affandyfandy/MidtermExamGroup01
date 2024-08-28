1a08140e3ff53b542bbfc9abdce9266cc7dd241f
manuellaiv <13521051@std.stei.itb.ac.id>
2024-08-28 12:12:04 +0700

    feat: init angular and base project

---
+++ frontend/src/app/config/app.constants.ts
@@ -0,0 +1,7 @@
+export const RouterConfig = {
+    HOME: {path: '', link: '/'},
+    LOGIN: {path: 'login', link: '/login', title: 'Login Page'},
+    PRODUCT: {path: 'product', link: '/product', title: 'Product Page'},
+    CUSTOMER: {path: 'customer', link: '/customer', title: 'Customer Page'},
+    INVOICE: {path: 'invoice', link: '/invoice', title: 'Invoice Page'},
+};
