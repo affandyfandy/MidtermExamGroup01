c845839bbe799cb567207cb90c8d82f6d4317b2e
manuellaiv <13521051@std.stei.itb.ac.id>
2024-08-28 20:27:03 +0700

    feat: header and footer

--- frontend/src/app/config/app.constants.ts
+++ frontend/src/app/config/app.constants.ts
@@ -1,5 +1,4 @@
 export const RouterConfig = {
-    HOME: {path: '', link: '/'},
     LOGIN: {path: 'login', link: '/login', title: 'Login Page'},
     PRODUCT: {path: 'product', link: '/product', title: 'Product Page'},
     CUSTOMER: {path: 'customer', link: '/customer', title: 'Customer Page'},
