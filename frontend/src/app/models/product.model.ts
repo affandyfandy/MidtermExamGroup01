10d9d50088ff1a28280195b6f3b5a9754d8ade00
manuellaiv <13521051@std.stei.itb.ac.id>
2024-08-28 21:19:14 +0700

    feat: models from springboot

--- frontend/src/app/models/product.model.ts
+++ frontend/src/app/models/product.model.ts
@@ -1,2 +1,12 @@
-export class Product {
+import { Status } from './status';
+import { InvoiceProduct } from './invoice-product.model';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  status: Status;
+  createdTime: Date;
+  updatedTime: Date;
+  invoiceProducts: InvoiceProduct[] | null;
 }
