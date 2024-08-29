74731f92bbb44add78bf2651e48e4e890506ca51
Irham Naufal <inaufal77@gmail.com>
2024-08-29 10:18:30 +0700

    feature: init customer table

--- frontend/src/app/models/customer.model.ts
+++ frontend/src/app/models/customer.model.ts
@@ -1,2 +1,8 @@
-export class Customer {
+export interface Customer {
+    id: number;
+    name: string;
+    phone_number: string;
+    status: 'ACTIVE' | 'INACTIVE';
+    created_time: Date;
+    updated_time: Date;
 }
