acab7a86a5cb88d99278c6b6562c8a91025f53d5
ryuzakijebi <ryuzaki.jebi@gmail.com>
2024-08-29 14:22:43 +0700

    feat: login.component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,36 +1,34 @@
 import { Routes } from '@angular/router';
 import { RouterConfig } from './config/app.constants';
+import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: RouterConfig.LOGIN.path,
-        loadChildren: () =>
-            import('./pages/login/login.routes')
-                .then(m => m.loginRoutes)
-    },
-    {
-        path: RouterConfig.PRODUCT.path,
-        loadChildren: () =>
-            import('./pages/product/product.routes')
-                .then(m => m.productRoutes)
-    },
-    {
-        path: RouterConfig.CUSTOMER.path,
-        loadChildren: () =>
-            import('./pages/customer/customer.routes')
-                .then(m => m.customerRoutes)
-    },
-    {
-        path: RouterConfig.INVOICE.path,
-        loadChildren: () =>
-            import('./pages/invoice/invoice.routes')
-                .then(m => m.invoiceRoutes)
-    },
-    {
-        path: `${RouterConfig.CUSTOMER.path}/:id`,
-        loadChildren: () =>
-            import('./pages/customer/customer.routes')
-                .then(m => m.customerRoutes)
-    },
-    { path: '**', redirectTo: '/' }
+  {
+    path: RouterConfig.LOGIN.path,
+    loadChildren: () =>
+      import('./pages/login/login.routes')
+        .then(m => m.loginRoutes)
+  },
+  {
+    path: RouterConfig.PRODUCT.path,
+    loadChildren: () =>
+      import('./pages/product/product.routes')
+        .then(m => m.productRoutes),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: RouterConfig.CUSTOMER.path,
+    loadChildren: () =>
+      import('./pages/customer/customer.routes')
+        .then(m => m.customerRoutes),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: RouterConfig.INVOICE.path,
+    loadChildren: () =>
+      import('./pages/invoice/invoice.routes')
+        .then(m => m.invoiceRoutes),
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '/login' }
 ];
